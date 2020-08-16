adb83aac146073f72aa58b573432c5efd96d1bb5
Chris <ctorresj08@gmail.com>
2020-08-16 09:45:41 -0500

    Init Commit

--- DataHandler.py
+++ DataHandler.py
@@ -10,33 +10,7 @@ class DataHandler:
     def __init__(self):
         self.user_list = {}
         self.schedule = list()
-
-    def save_to_file(self):
-        try:
-            directory = os.path.dirname(__file__)
-            file = os.path.join(directory, 'data.json')
-            data_file = Path(file)
-            with open(data_file, 'w') as f:
-                f.write(json.dumps(self.user_list, indent=4))
-                f.close()
-                return True
-        except JSONDecodeError as e:
-            print(f'{JSONDecodeError}: {e}')
-            return False
-
-    async def load_from_file(self):
-        try:
-            directory = os.path.dirname(__file__)
-            file = os.path.join(directory, 'data.json')
-            session_data_file = Path(file)
-            with open(session_data_file, 'r') as f:
-                data = json.load(f)
-                f.close()
-                self.user_list = data
-                return True
-        except JSONDecodeError as e:
-            print(f'{JSONDecodeError}: {e}')
-            return False
+        self.ping_role_name = "GDQping"
 
     def user_exists(self, user):
         id_str = str(user.id)
@@ -45,34 +19,24 @@ class DataHandler:
         else:
             return False
 
-    async def add_user(self, user):
-        if not self.user_exists(user):
-            self.user_list[str(user.id)] = {'muted': False}
-            self.save_to_file()
-            print(f'{user} as been added to the GDQ ping list')
+    async def add_user(self, user, ctx):
+        role = discord.utils.get(ctx.guild.roles, name=self.ping_role_name)
+        member = discord.utils.get(ctx.guild.members, id=user.id)
+        if role not in member.roles:
+            # self.user_list[str(user.id)] = {}
+            # self.save_to_file()
+            await member.add_roles(role)
+            print(f'{user} has been added to the GDQ ping list')
             return True
 
-    async def remove_user(self, user):
-        if self.user_exists(user):
-            self.user_list.pop(str(user.id))
+    async def remove_user(self, user, ctx):
+        role = discord.utils.get(ctx.guild.roles, name=self.ping_role_name)
+        member = discord.utils.get(ctx.guild.members, id=user.id)
+        if role in member.roles:
+            # self.user_list.pop(str(user.id))
+            await member.remove_roles(role)
             print(f'{user} has been removed the GDQ ping list')
             return True
-
-    async def mute(self, user):
-        if self.user_exists(user):
-            if self.user_list[str(user.id)]['muted'] is False:
-                self.user_list[str(user.id)]['muted'] = True
-                self.save_to_file()
-                print(f'{user} has MUTED the GDQ ping')
-                return True
-
-    async def unmute(self, user):
-        if self.user_exists(user):
-            if self.user_list[str(user.id)]['muted'] is True:
-                self.user_list[str(user.id)]['muted'] = False
-                self.save_to_file()
-                print(f'{user} has UNMUTED the GDQ ping')
-                return True
 
     async def get_schedule(self, ctx):
         limit = 4
@@ -81,7 +45,7 @@ class DataHandler:
         if len(self.schedule) < 4:
             limit = len(self.schedule)
 
-        schedule_embed = discord.Embed(title="GAMES DONE QUICK 2020", url="https://gamesdonequick.com/schedule",
+        schedule_embed = discord.Embed(title="GAMES DONE QUICK 2020 - Coming up", url="https://gamesdonequick.com/schedule",
                                        description=" Please click title for full schedule", color=0x466e9c)
         schedule_embed.set_thumbnail(url="https://gamesdonequick.com/static/res/img/gdqlogo.png")
         now = datetime.now()
@@ -95,7 +59,7 @@ class DataHandler:
         else:
             time_remaining = self.diffdates(now, run_datetime)
             schedule_embed.add_field(
-                name=f'{time_remaining} until {self.schedule[0]["game"]} ({self.schedule[0]["run"]})',
+                name=f'{self.schedule[0]["game"]} ({self.schedule[0]["run"]}) in {time_remaining}',
                 value=f'By: {self.schedule[0]["runners"]} | '
                       f'Estimated length: {self.explodeminutes(self.schedule[0]["length"])}',
                 inline=False)
@@ -105,7 +69,7 @@ class DataHandler:
             run_datetime = self.strtodatetime(self.schedule[run_pointer]["time"])
             time_remaining = self.diffdates(now, run_datetime)
             schedule_embed.add_field(
-                name=f'{time_remaining} until {self.schedule[run_pointer]["game"]} ({self.schedule[run_pointer]["run"]})',
+                name=f'{self.schedule[run_pointer]["game"]} ({self.schedule[run_pointer]["run"]}) in {time_remaining}',
                 value=f'By: {self.schedule[run_pointer]["runners"]} | '
                       f'Estimated length: {self.explodeminutes(self.schedule[run_pointer]["length"])}',
                 inline=False)
@@ -121,17 +85,17 @@ class DataHandler:
     @staticmethod
     def diffdates(now, later):
         diff = later - now
-        print(f'{later + diff}')
         hours = divmod(diff.total_seconds(), 3600)[0]
         minutes = divmod(diff.total_seconds(), 60)[0] - (hours * 60)
-        return f'{hours:.0f} hrs {minutes:.0f} minutes'
+        seconds = diff.total_seconds() - (hours * 3600) - (minutes * 60)
+        return f'{hours:.0f}h {round(minutes):.0f}m {seconds:.0f}s'
 
     @staticmethod
     def explodeminutes(minutes):
         hours = int(minutes / 60)
         minutes = minutes - (hours * minutes)
 
-        return f'{hours}:{minutes}'
+        return f'{hours}h {minutes}m'
 
     async def load_schedule(self):
         try:
@@ -146,3 +110,44 @@ class DataHandler:
         except JSONDecodeError as e:
             print(f'{JSONDecodeError}: {e}')
             return False
+
+    async def save_schedule(self):
+        try:
+            directory = os.path.dirname(__file__)
+            file = os.path.join(directory, 'gdq2020schedule.json')
+            data_file = Path(file)
+            with open(data_file, 'w') as f:
+                f.write(json.dumps(self.schedule, indent=4))
+                f.close()
+                return True
+        except JSONDecodeError as e:
+            print(f'{JSONDecodeError}: {e}')
+            return False
+    '''
+       async def load_from_file(self):
+           try:
+               directory = os.path.dirname(__file__)
+               file = os.path.join(directory, 'data.json')
+               session_data_file = Path(file)
+               with open(session_data_file, 'r') as f:
+                   data = json.load(f)
+                   f.close()
+                   self.user_list = data
+                   return True
+           except JSONDecodeError as e:
+               print(f'{JSONDecodeError}: {e}')
+               return False
+
+            async def save_to_file(self):
+        try:
+            directory = os.path.dirname(__file__)
+            file = os.path.join(directory, 'data.json')
+            data_file = Path(file)
+            with open(data_file, 'w') as f:
+                f.write(json.dumps(self.user_list, indent=4))
+                f.close()
+                return True
+        except JSONDecodeError as e:
+            print(f'{JSONDecodeError}: {e}')
+            return False
+       '''
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import asyncio
+import json
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
+from datetime import datetime, timedelta
+from pathlib import Path
+from json import JSONDecodeError
 from DataHandler import DataHandler
 
 load_dotenv()
@@ -15,6 +19,61 @@ bot = commands.Bot(command_prefix='+')
 data_handler = DataHandler()
 
 
+async def schedule_check_clock():
+    while True:
+
+        for run in data_handler.schedule:
+            run_datetime = data_handler.strtodatetime(run["time"])
+            reminder_time = run_datetime - timedelta(hours=0, minutes=5)
+            end_of_run = run_datetime + timedelta(hours=0, minutes=run["length"])
+
+            if reminder_time < datetime.now() < end_of_run and run["reminded"] is False:
+                # ping squad
+                run["reminded"] = True
+                await data_handler.save_schedule()
+                time_remaining = data_handler.diffdates(datetime.now(), run_datetime)
+                reminder_embed = discord.Embed(title="STARTING SOON - GAMES DONE QUICK 2020",
+                                               url="https://gamesdonequick.com/schedule",
+                                               description=" Please click title for full schedule", color=0x466e9c)
+                reminder_embed.set_thumbnail(url="https://gamesdonequick.com/static/res/img/gdqlogo.png")
+                reminder_embed.add_field(
+                    name=f'{run["game"]} ({run["run"]}) in {time_remaining}',
+                    value=f'By: {run["runners"]} | '
+                          f'Estimated length: {data_handler.explodeminutes(run["length"])}',
+                    inline=False)
+
+                channel_list = None
+
+                try:
+                    directory = os.path.dirname(__file__)
+                    file = os.path.join(directory, 'channels.json')
+                    session_data_file = Path(file)
+                    with open(session_data_file, 'r') as f:
+                        data = json.load(f)
+                        f.close()
+                        channel_list = data
+                except JSONDecodeError as e:
+                    print(f'{JSONDecodeError}: {e}')
+
+                for channel in channel_list:
+                    print(channel)
+                    channel_to_send_to = bot.get_channel(channel)
+                    if channel_to_send_to is not None:
+                        role = discord.utils.get(channel_to_send_to.guild.roles, name=data_handler.ping_role_name)
+                        message_content = ""
+                        if role is not None:
+                            message_content = role.mention
+                        await channel_to_send_to.send(message_content, embed=reminder_embed)
+
+                break
+            elif datetime.now() > end_of_run:
+                # remove run
+                data_handler.schedule.remove(run)
+                await data_handler.save_schedule()
+
+        await asyncio.sleep(30)
+
+
 @bot.event
 async def on_ready():
     # Show connected guilds
@@ -24,6 +83,7 @@ async def on_ready():
         print(f'{g.name} | (id: {g.id})')
     print()
 
+    '''
     print('Loading user list...')
     load_result = await data_handler.load_from_file()
     if load_result is True:
@@ -31,55 +91,34 @@ async def on_ready():
         print(data_handler.user_list)
     else:
         print('Error loading user list!')
-
+    '''
     print('Loading schedule...')
     load_result = await data_handler.load_schedule()
     if load_result is True:
         print('Done')
-        print(data_handler.schedule)
     else:
         print('Error loading schedule!')
 
+    await bot.loop.create_task(schedule_check_clock())
+
+
 @bot.command(name='add', help='Add yourself to the ping squad')
 async def add(ctx, *args):
-    result = await data_handler.add_user(ctx.author)
+    result = await data_handler.add_user(ctx.author, ctx)
     if result is True:
         await ctx.send(f'{ctx.author} has been added to the GDQ ping list')
-        print(data_handler.user_list)
 
 
 @bot.command(name='remove', help='Remove yourself from the ping squad')
 async def remove(ctx, *args):
-    result = await data_handler.remove_user(ctx.author)
+    result = await data_handler.remove_user(ctx.author, ctx)
     if result is True:
         await ctx.send(f'{ctx.author} has been removed from the GDQ ping list')
-        print(data_handler.user_list)
-
-
-@bot.command(name='mute', help='Mute Notifications')
-async def mute(ctx, *args):
-    result = await data_handler.mute(ctx.author)
-    if result is True:
-        await ctx.send(f'{ctx.author} has MUTED the GDQ ping')
-        print(data_handler.user_list[str(ctx.author.id)])
-
-
-@bot.command(name='unmute', help='Unmute Notifications')
-async def unmute(ctx, *args):
-    result = await data_handler.unmute(ctx.author)
-    if result is True:
-        await ctx.send(f'{ctx.author} has UNMUTED the GDQ ping')
-        print(data_handler.user_list[str(ctx.author.id)])
 
 
 @bot.command(name='schedule', help='See GDQ schedule')
 async def schedule(ctx, *args):
     await data_handler.get_schedule(ctx)
-
-
-@bot.command(name='hi', help='-')
-async def hi(ctx, *args):
-    await ctx.send("Hello!")
 
 
 bot.run(TOKEN)
